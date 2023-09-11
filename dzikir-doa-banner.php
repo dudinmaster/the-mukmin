9ea17b27015a3870214eea5bc7eed9a6fe21d3f8
Ahmad Imaddudin <imaddudina@gmail.com>
2023-09-11 16:30:29 +0700

    copy folder project
    clone folder dari punya nabila

---
+++ dzikir-doa-banner.php
@@ -0,0 +1,72 @@
+<!-- Activities Start -->
+<div class="container-fluid activities py-5">
+            <div class="container py-5">
+                <div class="mx-auto text-center mb-5 wow fadeIn" data-wow-delay="0.1s" style="max-width: 700px;">
+                    <p class="fs-5 text-uppercase text-primary">Dzikir & Doa</p>
+                    <h1 class="display-3">Daftar Dzikir & Doa</h1>
+                </div>
+                <div class="row g-4">
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.1s">
+                            <i class="fa fa-mosque fa-4x text-dark"></i>
+                            <div class="ms-4">
+                                <h4>Mosque Development</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
+                                <a href="" class="btn btn-primary px-3">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.3s">
+                            <i class="fa fa-donate fa-4x text-dark"></i>
+                            <div class="ms-4">
+                                <h4>Charity & Donation</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
+                                <a href="" class="btn btn-primary px-3">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.5s">
+                            <i class="fa fa-quran fa-4x text-dark"></i>
+                            <div class="ms-4">
+                                <h4>Quran Learning</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
+                                <a href="" class="btn btn-primary px-3">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.1s">
+                            <i class="fa fa-book fa-4x text-dark"></i>
+                            <div class="ms-4">
+                                <h4>Hadith & Sunnah</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
+                                <a href="" class="btn btn-primary px-3">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.3s">
+                            <i class="fa fa-book-open fa-4x text-dark"></i>
+                            <div class="ms-4">
+                                <h4>Parent Education</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
+                                <a href="" class="btn btn-primary px-3">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-4">
+                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.5s">
+                            <i class="fa fa-hands fa-4x text-dark"></i>
+                            <div class="ms-4">
+                                <h4>Help Orphans</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
+                                <a href="" class="btn btn-primary px-3">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Activities Start -->
