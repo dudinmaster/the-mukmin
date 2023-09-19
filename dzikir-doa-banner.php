f01aef8947d2c3a5397cd023f4cac31f3d5ad1ef
Ahmad Imaddudin <imaddudina@gmail.com>
2023-09-19 15:46:20 +0700

    make trim teks feature
    membuat fitur read more menggunakan fungsi php substr

--- dzikir-doa-banner.php
+++ dzikir-doa-banner.php
@@ -1,3 +1,5 @@
+<?php include "config.php"; ?>
+
 <!-- Activities Start -->
 <div class="container-fluid activities py-5">
             <div class="container py-5">
@@ -5,67 +7,28 @@
                     <p class="fs-5 text-uppercase text-primary">Dzikir & Doa</p>
                     <h1 class="display-3">Daftar Dzikir & Doa</h1>
                 </div>
+
+                <?php 
+                    $sql = "select * from daftardoa" ; 
+                    $result = mysqli_query($conn,$sql);
+
+                    //while($row = mysqli_fetch_assoc($result)) {
+                ?>
                 <div class="row g-4">
+                    <?php  while($row = mysqli_fetch_assoc($result)) { 
+                        $trim_text = substr($row['isi_konten'],0,350);    
+                    ?>
                     <div class="col-lg-6 col-xl-4">
                         <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.1s">
                             <i class="fa fa-mosque fa-4x text-dark"></i>
                             <div class="ms-4">
-                                <h4>Mosque Development</h4>
-                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
+                                <h4><?= $row['judul'];?></h4>
+                                <p class="mb-4"><?= $trim_text . '...' ;?></p>
                                 <a href="" class="btn btn-primary px-3">Read More</a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6 col-xl-4">
-                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.3s">
-                            <i class="fa fa-donate fa-4x text-dark"></i>
-                            <div class="ms-4">
-                                <h4>Charity & Donation</h4>
-                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
-                                <a href="" class="btn btn-primary px-3">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xl-4">
-                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.5s">
-                            <i class="fa fa-quran fa-4x text-dark"></i>
-                            <div class="ms-4">
-                                <h4>Quran Learning</h4>
-                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
-                                <a href="" class="btn btn-primary px-3">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xl-4">
-                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.1s">
-                            <i class="fa fa-book fa-4x text-dark"></i>
-                            <div class="ms-4">
-                                <h4>Hadith & Sunnah</h4>
-                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
-                                <a href="" class="btn btn-primary px-3">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xl-4">
-                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.3s">
-                            <i class="fa fa-book-open fa-4x text-dark"></i>
-                            <div class="ms-4">
-                                <h4>Parent Education</h4>
-                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
-                                <a href="" class="btn btn-primary px-3">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-xl-4">
-                        <div class="activities-item p-4 wow fadeIn" data-wow-delay="0.5s">
-                            <i class="fa fa-hands fa-4x text-dark"></i>
-                            <div class="ms-4">
-                                <h4>Help Orphans</h4>
-                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Donec tempus eros vel dolor mattis aliquam.</p>
-                                <a href="" class="btn btn-primary px-3">Read More</a>
-                            </div>
-                        </div>
-                    </div>
+                    <?php } ?> 
                 </div>
             </div>
         </div>
