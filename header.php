e2443b23c017390aa23d1a2cb473c50395e2fb3a
Ahmad Imaddudin <imaddudina@gmail.com>
2023-09-26 17:21:56 +0700

    uji test komen section

--- header.php
+++ header.php
@@ -26,6 +26,9 @@
 
         <!-- Template Stylesheet -->
         <link href="css/style.css" rel="stylesheet">
+        <!-- <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"> -->
+        <link href="css/comment.css" rel="stylesheet"> 
 
         
 
